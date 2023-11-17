b9a8e73f9dd997443759d6d487c2c4d47e6024c8
Catalin Halic <catalin.halic@gmail.com>
2023-11-18 00:08:56 +0200

    Initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,44 @@
+import {Component} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  title: string = environment.appName;
+  logo: string = environment.appLogo;
+  owner: string = environment.appOwner;
+
+  items: Array<any> = [
+    {
+      title: 'Home',
+      route: '/home',
+    },
+    {
+      title: 'Admin',
+      route: '/dashboard',
+    },
+    {
+      title: 'Logout',
+      route: '/auth',
+    },
+  ];
+
+  imageSalesLeft: string = 'assets/sales_left.jpeg';
+  imageSalesMiddle: string = 'assets/sales_middle.jpeg';
+  imageSalesRight: string = 'assets/sales_right.jpeg';
+
+  constructor(private router: Router) {
+  }
+
+  onChangePage(page: any) {
+    this.router.navigateByUrl(page.route);
+  }
+
+  onLogout(): void {
+    this.router.navigateByUrl('/auth')
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+// generated with ng g environments
+
+export const environment = {
+  apiUrl:"http://localhost:8080",
+  appName: "Application name",
+  appOwner: "Username",
+  appLogo: "https://www.creativefabrica.com/wp-content/uploads/2019/02/Online-shop-shopping-shop-logo-by-DEEMKA-STUDIO-3.jpg"
+};
